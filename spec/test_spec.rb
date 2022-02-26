c944f522e1782e3d9d25a7e0fe5ae71dbf4c2f38
mokumoku-chan <maho.kumo1997@gmail.com>
2022-02-26 14:39:49 +0000

    Add subtraction

--- spec/test_spec.rb
+++ spec/test_spec.rb
@@ -10,4 +10,16 @@ describe '四則演算' do
       expect(1 + 1).to eq 3
     end
   end
+
+  context '引き算' do
+    it '6-2は4になる' do
+      expect(6-2).to eq 4
+    end
+  end
+
+  context '掛け算' do
+    it '6*2は12になる' do
+      expect(6*2).to eq 10
+    end
+  end
 end
